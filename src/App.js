b53ded0a1681198f130ecf6d0ed65bedf822c939
dianannperla <waafirmelyza@hotmail.com>
2025-10-03 17:35:24 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trang Chatbot</h1>
+      <p>Hãy hỏi tôi bất kỳ điều gì!</p>
+    </div>
+  );
+}
+
+export default App;
