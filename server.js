2355f17964617669373d88bd19b1ecb7895ad2a0
dianannperla <waafirmelyza@hotmail.com>
2025-10-03 17:36:22 +0100

    Update welcome message for clarity.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome! This is the chatbot website.');
+  res.send('Welcome to our chatbot platform!');
 });
 
 app.listen(PORT, () => {
